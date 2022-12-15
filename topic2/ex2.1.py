0a945d0ea6877daddb44ea00b435b91c123dcd4a
koker342 <koker342@yandex.ru>
2022-12-15 23:30:42 +0300

    исправлены ошибки

--- topic2/ex2.1.py
+++ topic2/ex2.1.py
@@ -2,7 +2,7 @@ room_temp = int(input())
 desired_temp = int(input())
 humidity = int(input())
 
-if room_temp > desired_temp and humidity > 80:
+if room_temp > desired_temp and humidity >= 80:
     print('on')
 else:
     print('off')    
